2a0d803f6df3a4d28e63650d1b662ad2e56d68a4
Jaindris Sosa <jaindrissosajsd@gmail.com>
2023-02-08 09:29:59 -0500

    Add a function to count the number of monkeys

--- ejercicios/ejercicios.js
+++ ejercicios/ejercicios.js
@@ -11,4 +11,23 @@ function DNAtoRNA(dna = "") {
 
 console.log(DNAtoRNA("GCA"));
 
-//  Serie de ejercicios resultos de las paginas codewars leetcode hackerrank entre otras 
+// You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+// As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+// For example(Input --> Output):
+// 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//  1 --> [1]
+
+function monkeyCount(n) {
+  // your code here
+
+  let result = [];
+  let i = 0;
+  while (i < n) {
+    i++;
+    result.push(i);
+  }
+
+  return result;
+}
+
+console.log(monkeyCount(5));
